10fd4ed199fc4faeb36391c566c8c11a23412255
Mohit Tater <mohittater@bluejeansnet.com>
2015-05-24 19:03:22 +0530

    Changes for making table row options i.e. delete and sharing features

--- FoodPin/RestaurantTableTableViewController.swift
+++ FoodPin/RestaurantTableTableViewController.swift
@@ -83,53 +83,59 @@ class RestaurantTableTableViewController: UITableViewController {
     }
 
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle:UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
             // Delete the row from the data source
+            self.restaurantIsVisited.removeAtIndex(indexPath.row)
+            self.restaurantImages.removeAtIndex(indexPath.row)
+            self.restaurantLocations.removeAtIndex(indexPath.row)
+            self.restaurantNames.removeAtIndex(indexPath.row)
+            self.restaurantTypes.removeAtIndex(indexPath.row)
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
+
+        }
+        else if editingStyle == .Insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
 
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
+// add actions for row elements of tableView this function will return delete and share function for the row element
+    override func tableView(tableView: UITableView, editActionsForRowAtIndexPath indexPath:
+            NSIndexPath) -> [AnyObject] {
+            var shareAction = UITableViewRowAction(style: UITableViewRowActionStyle.Default, title:
+            "Share", handler: { (action:UITableViewRowAction!, indexPath:NSIndexPath!) -> Void in
+            
+            let shareMenu = UIAlertController(title: nil, message: "Share using",
+        preferredStyle: .ActionSheet)
+            let twitterAction = UIAlertAction(title: "Twitter", style:
+        UIAlertActionStyle.Default, handler: nil)
+            let facebookAction = UIAlertAction(title: "Facebook", style:
+        UIAlertActionStyle.Default, handler: nil)
+            let emailAction = UIAlertAction(title: "Email", style: UIAlertActionStyle.Default,
+        handler: nil)
+            let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel,
+        handler: nil)
+            shareMenu.addAction(twitterAction)
+            shareMenu.addAction(facebookAction)
+            shareMenu.addAction(emailAction)
+            shareMenu.addAction(cancelAction)
+            self.presentViewController(shareMenu, animated: true, completion: nil)
+            } )
+            
+            var deleteAction = UITableViewRowAction(style: UITableViewRowActionStyle.Default,
+    title: "Delete",handler: { (action:UITableViewRowAction!, indexPath:NSIndexPath!) -> Void in
+    // Delete the row from the data source
+    self.restaurantNames.removeAtIndex(indexPath.row)
+    self.restaurantLocations.removeAtIndex(indexPath.row)
+    self.restaurantTypes.removeAtIndex(indexPath.row)
+    self.restaurantIsVisited.removeAtIndex(indexPath.row)
+    self.restaurantImages.removeAtIndex(indexPath.row)
+    self.tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+    } )
+            return [deleteAction, shareAction]
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
-
     
-override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
     let optionMenu =
         UIAlertController(title: nil, message: "What do you want to do?", preferredStyle: .ActionSheet)
         
@@ -188,5 +194,6 @@ override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPat
     override func  prefersStatusBarHidden() -> Bool{
             return true;
     }
+    
 
 }
